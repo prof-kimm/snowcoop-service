8ec2a4ab823d7d2e427930e472c6c5f30db03011
prof-kimm <kimm@algonquincollege.com>
2019-04-14 16:07:09 -0400

    adding auth guard

--- src/modules/address/address.controller.ts
+++ src/modules/address/address.controller.ts
@@ -1,23 +1,26 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { JwtPayload, UserPayload } from 'common/interfaces';
 import { CreateAddressDto } from 'common/dtos/create-address.dto';
 import { AddressService } from './address.service';
 import { Address } from './address.entity';
-
+import { AuthGuard } from '@nestjs/passport';
+import { AuthUser } from 'common/decorators/user.decorator';
+import { User } from '../user/user.entity';
 
 @Controller('address')
 export class AddressController {
-  constructor(
-    private readonly addressService: AddressService,
-  ) { }
+  constructor(private readonly addressService: AddressService) {}
 
   @Get('')
-  async login(): Promise<Address[]> {
+  @UseGuards(AuthGuard('jwt'))
+  async getAddressList(@AuthUser() authUser: User): Promise<Address[]> {
     return await this.addressService.findAll();
   }
 
   @Post('')
-  async register(
+  @UseGuards(AuthGuard('jwt'))
+  async createAddress(
+    @AuthUser() authUser: User,
     @Body() createAddressDto: CreateAddressDto,
   ): Promise<any> {
     return await this.addressService.create(createAddressDto);
